3580457e5d2d2f7d9530a6bb1d317172111e8d84
galaxy <690794510@qq.com>
2019-01-09 16:48:47 +0800

    dev第二次修改

--- HelloWorldDEMO/src/helloworlddemo/Activator.java
+++ HelloWorldDEMO/src/helloworlddemo/Activator.java
@@ -10,7 +10,7 @@ public class Activator implements BundleActivator {       //��Ҫ����
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {       //��ں���
-		System.out.println("Hello World!!1111");
+		System.out.println("Hello World!!");
 	}
 	
 	/*
