b03baac58070c3f4f4e49e6a281d50a3470e51da
galaxy <690794510@qq.com>
2019-01-09 16:18:37 +0800

    dev002第一次修改

---
+++ HelloWorldDEMO/src/helloworlddemo/AddDEV02.java
@@ -0,0 +1,11 @@
+package helloworlddemo;
+
+public class AddDEV02 {
+
+	public int add(int a, int b) {
+		int c = a * b;
+		System.out.println(c);
+		return c;
+	}
+
+}
--- HelloWorldDEMO/src/helloworlddemo/DevBranch.java
+++
@@ -1,9 +0,0 @@
-package helloworlddemo;
-
-public class DevBranch {
-	public float multiply(float a, float b) {
-		float c = a * b;
-		System.out.println(c);
-		return c;
-	}
-}
